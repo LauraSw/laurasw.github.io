acdb39e6a1e5b95f5e8e28f56197469b7c7b0b34
laurasw <schwenk_laura@web.de>
2019-11-17 12:55:42 +0100

    neu

---
+++ Aufgabe_5/meinescriptdatei.js
@@ -0,0 +1,9 @@
+var test = "Hallo";
+var welt = "Welt";
+var zahl1 = 200;
+var zahl2 = 20;
+var ergebnis = (zahl2 / zahl1) * 100;
+var prozent = "%";
+console.log(ergebnis + prozent);
+console.log(ergebnis + "%");
+//# sourceMappingURL=meinescriptdatei.js.map
---
+++ Aufgabe_5/meinescriptdatei.ts
@@ -0,0 +1,9 @@
+var test:string = "Hallo";
+var welt:string = "Welt";
+
+var zahl1:number = 200;
+var zahl2:number = 20;
+var ergebnis:number = (zahl2/zahl1)*100;
+var prozent:string = "%";
+console.log(ergebnis+prozent);
+console.log(ergebnis+"%")
