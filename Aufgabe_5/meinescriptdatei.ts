b6291f6ecaa31e90f55c412dffa9538931535b60
laurasw <schwenk_laura@web.de>
2019-11-17 15:13:56 +0100

    aufgabe5

--- Aufgabe_5/meinescriptdatei.ts
+++ Aufgabe_5/meinescriptdatei.ts
@@ -1,9 +1,70 @@
-var test:string = "Hallo";
-var welt:string = "Welt";
+var afrika:string = "AFRIKA"
+var südamerika:string = "SÜDAMERIKA"
+var europa:string = "EUROPA"
+var nordamerika:string = "NORDAMERIKA"
+var asien:string = "ASIEN"
 
-var zahl1:number = 200;
-var zahl2:number = 20;
-var ergebnis:number = (zahl2/zahl1)*100;
-var prozent:string = "%";
-console.log(ergebnis+prozent);
-console.log(ergebnis+"%")
+var afrikaemission2018:number = 1235.5;
+var südamerikaemission2018:number = 1261.5;
+var europaemission2018:number = 4209.3;
+var nordamerikaemission2018:number = 6035.6;
+var asienemission2018:number = 16274.1;
+
+var afrikaemission2008:number = 1028;
+var südamerikaemission2008:number = 1132.6;
+var europaemission2008:number = 4965.7;
+var nordamerikaemission2008:number = 6600.4;
+var asienemission2008:number = 12954.7;
+
+var gesamtemission2018: number = afrikaemission2018+europaemission2018+südamerikaemission2018+nordamerikaemission2018+asienemission2018;
+
+
+
+var anteilafrika2018:number = (afrikaemission2018/gesamtemission2018)*100;
+var wachstumafrikavon2008zu2018absolut:number = afrikaemission2018-afrikaemission2008;
+var wachstumafrikavon2008zu2018prozentual:number = -(wachstumafrikavon2008zu2018absolut/afrikaemission2008)*100;
+console.log(afrika);
+console.log("Die Emission von Afrika ist: "+afrikaemission2018+"kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verursacht Afrika damit "+anteilafrika2018+"%.");
+console.log("Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um "+wachstumafrikavon2008zu2018prozentual+"% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das "+wachstumafrikavon2008zu2018absolut+"kg CO2.");
+
+
+console.log(südamerika);
+console.log("Die Emission von Südamerika ist: "+südamerikaemission2018+"kg CO2.");
+var anteilsüdamerika2018:number = (südamerikaemission2018/gesamtemission2018)*100;
+console.log("Relativ zur Gesamtemission der Welt verursacht Südamerika damit "+anteilsüdamerika2018+"%.");
+var wachstumsüdamerikavon2008zu2018absolut:number = südamerikaemission2018-südamerikaemission2008;
+var wachstumsüdamerikavon2008zu2018prozentual:number = -(wachstumsüdamerikavon2008zu2018absolut/südamerikaemission2008)*100;
+console.log("Für Südamerika hat sich 2018 im Vergleich zu 2008 die Emission um "+wachstumsüdamerikavon2008zu2018prozentual+"% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das "+wachstumsüdamerikavon2008zu2018absolut+"kg CO2.");
+
+
+console.log(europa);
+console.log("Die Emission von Europa ist: "+europaemission2018+"kg CO2.");
+var anteileuropa2018:number = (europaemission2018/gesamtemission2018)*100;
+console.log("Relativ zur Gesamtemission der Welt verursacht Europa damit "+anteileuropa2018+"%.");
+var wachstumeuropavon2008zu2018absolut:number = europaemission2018-europaemission2008;
+var wachstumeuropavon2008zu2018prozentual:number = -(wachstumeuropavon2008zu2018absolut/europaemission2008)*100;
+console.log("Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um "+wachstumeuropavon2008zu2018prozentual+"% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das "+wachstumeuropavon2008zu2018absolut+"kg CO2.");
+
+
+console.log(nordamerika);
+console.log("Die Emission von Nordamerika ist: "+nordamerikaemission2018+"kg CO2.");
+var anteilnordamerika2018:number = (nordamerikaemission2018/gesamtemission2018)*100;
+console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit "+anteilnordamerika2018+"%.");
+var wachstumnordamerikavon2008zu2018absolut:number = nordamerikaemission2018-nordamerikaemission2008;
+var wachstumnordamerikavon2008zu2018prozentual:number = -(wachstumnordamerikavon2008zu2018absolut/nordamerikaemission2008)*100;
+console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um "+wachstumnordamerikavon2008zu2018prozentual+"% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das "+wachstumnordamerikavon2008zu2018absolut+"kg CO2.");
+
+
+console.log(asien);
+console.log("Die Emission von Asien ist: "+asienemission2018+"kg CO2.");
+var anteilasien2018:number = (asienemission2018/gesamtemission2018)*100;
+console.log("Relativ zur Gesamtemission der Welt verursacht Asien damit "+anteilasien2018+"%.");
+var wachstumasienvon2008zu2018absolut:number = asienemission2018-asienemission2008;
+var wachstumasienvon2008zu2018prozentual:number = -(wachstumasienvon2008zu2018absolut/asienemission2008)*100;
+console.log("Für Asien hat sich 2018 im Vergleich zu 2008 die Emission um "+wachstumasienvon2008zu2018prozentual+"% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das "+wachstumasienvon2008zu2018absolut+"kg CO2.");
