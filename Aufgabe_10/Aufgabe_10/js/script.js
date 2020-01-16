f3cc834a3219c7c9690e416c8a9eebd6a14e3ced
laurasw <schwenk_laura@web.de>
2020-01-16 01:09:43 +0100

    10

---
+++ Aufgabe_10/Aufgabe_10/js/script.js
@@ -0,0 +1,239 @@
+/**
+ * Die ToDos werden in dem Array todosText gespeichert
+ * Jedes ToDo hat aber, neben dem ToDo-Text, einen zweiten
+ * Wert, nämlich ob es erledigt ist oder nicht
+ * (checked / unchecked). Der Einsatz von einem eindimensionalen
+ * Array ermöglicht aber nur, dass wir ein Wert nach dem anderen auflisten.
+ * Den zweiten Wert zu einem ToDo speichern wir also in einem
+ * zweiten Array. Beide Arrays beinhalten also Infos zu einem ToDo,
+ * den ToDo-Text und den Erledigtstatus eines ToDos. Die entsprechende
+ * Stelle eines ToDos zeigt jetzt in den Arrays auf die entsprechenden
+ * Werte, bspw. Stelle 0 im Array todosText und Stelle 0 im Array
+ * todosChecked gehören zusammen zu einem ToDo.
+ */
+var allTasks = [];
+/**
+ * Die Anwendung wird immer wieder auf die selben
+ * DOM-Elemente zugreifen müssen. Damit diese Elemente nicht
+ * jedes mal neu selektiert werden müssen, werden hier
+ * Variablen deklariert, die später die entsprechenden DOM-Elemente
+ * speichern.
+ */
+var inputDOMElement;
+var addButtonDOMElement;
+var todosDOMElement;
+var counterDOMElement;
+var openCounterDOMElement;
+var doneCounterDOMElement;
+var counterDone;
+var counterOpen;
+var newTask;
+/**
+ * Sobald der DOM geladen wurde können grundlegende DOM-Interaktionen
+ * initialisiert werden
+ */
+window.addEventListener("load", function () {
+    /**
+     * Jetzt da der DOM verfügbar ist können die wichtigsten Elemente
+     * in ihre Variablen gespeichert werden, um später auf sie
+     * zugreifen zu können
+     */
+    inputDOMElement = document.querySelector("#inputTodo");
+    addButtonDOMElement = document.querySelector("#addButton");
+    todosDOMElement = document.querySelector("#todos");
+    counterDOMElement = document.querySelector("#counter");
+    openCounterDOMElement = document.querySelector("#openCounter");
+    doneCounterDOMElement = document.querySelector("#doneCounter");
+    /**
+     * Jetzt da der DOM verfügbar ist kann auch ein Event-Listener
+     * auf den AddToDo Button gesetzt werden.
+     */
+    addButtonDOMElement.addEventListener("click", addTodo);
+    /**
+     * Initial soll einmal die Liste an bereit definierten ToDos
+     * aus den Arrays in den DOM gezeichnet werden.
+     */
+    drawListToDOM();
+});
+function drawListToDOM() {
+    // alle todos erst einmal aus dem DOM löschen
+    todosDOMElement.innerHTML = "";
+    var _loop_1 = function (index) {
+        /**
+         * Neues DIV-Element erstellen (würde auch mit innerHTML = "<div class='todo'></div>" gehen,
+         * die Objekt-Instansierung ist aber übersichtlicher)
+         */
+        var todo = document.createElement("div");
+        todo.classList.add("todo");
+        /**
+         * Jedes Todo besteht aus etwas Markup, also aus HTML-Elementen
+         * wie der Check-Anzeige, dem ToDo-Text und dem Mülleimer
+         *
+         * Einfachheitshalber werden hier alle HTML-Elemente für ein ToDo
+         * nicht DOM-Objekt-weise (wie oben, mit createElement), sondern als eine lange
+         * HTML-Zeichenkette erstellt. An manchen Stellen der Zeichenkette wird
+         * ein Wert einer Variablen benötigt (bspw. für die CSS Klasse oder für den ToDo-Text),
+         * hier muss die Zeichenkette unterbrochen werden.
+         */
+        todo.innerHTML = "<span class='check " + allTasks[index].todosChecked + "'><i class='fas fa-check'></i></span>"
+            + allTasks[index].todosText +
+            "<span class='trash fas fa-trash-alt'></span>";
+        // Zuweisen der Event-Listener für den Check- und den Trash-Button
+        todo.querySelector(".check").addEventListener("click", function () {
+            // hier wird der Index, also die aktuelle Stelle im Array dieses ToDos,
+            // übergeben, damit an der entsprechenden Stelle im Array der Wert geändert werden kann.
+            toggleCheckState(index);
+        });
+        todo.querySelector(".trash").addEventListener("click", function () {
+            // hier wird der Index, also die aktuelle Stelle im Array dieses ToDos,
+            // übergeben, damit die entsprechende Stelle im Array gelöscht werden kann.
+            deleteTodo(index);
+        });
+        // Bis hier hin wurde das neue Todo "zusammengebaut", jetzt wird es in den DOM gerendert.
+        todosDOMElement.appendChild(todo);
+    };
+    // das ToDo-Array durchlaufen (iterieren) und Todo für Todo in den DOM schreiben
+    for (var index = 0; index < allTasks.length; index++) {
+        _loop_1(index);
+    }
+    updateCounter();
+    updateOpenCounter();
+    updateDoneCounter();
+}
+function updateCounter() {
+    counterDOMElement.innerHTML = allTasks.length + " in total";
+}
+function updateOpenCounter() {
+    counterOpen = 0;
+    for (var i = 0; i < allTasks.length; i++) {
+        if (allTasks[i].todosChecked == false) {
+            counterOpen++;
+        }
+    }
+    openCounterDOMElement.innerHTML = counterOpen + " open";
+}
+function updateDoneCounter() {
+    counterDone = 0;
+    for (var z = 0; z < allTasks.length; z++) {
+        if (allTasks[z].todosChecked == true) {
+            counterDone++;
+        }
+    }
+    doneCounterDOMElement.innerHTML = counterDone + " done";
+}
+/**
+ * Ein neues ToDo wird folgendermaßen erstellt:
+ */
+function addTodo() {
+    /**
+     * Zunächst wird geprüft, ob das Input-Feld nicht leer ist
+     * (ansonsten würde ein leerer ToDo-Text erstellt werden,
+     * wenn man, ohne zu Tippen, den Add-Button gedrückt hätte)
+     */
+    if (inputDOMElement.value != "") {
+        /**
+         * Der Eingabe-Wert aus dem Input-Feld (.value) wird
+         * als neues Element in das ToDo-Array gepusht.
+         * Gleichzeitig wird in ein zweites Array, das den Checked- / Uncheck-
+         * Status der ToDos abbildet, für dieses ToDo (weil selbe Stelle im Array)
+         * der Status "unchecked", hier false, gepusht.
+         */
+        newTask = { todosText: inputDOMElement.value, todosChecked: false };
+        allTasks.unshift(newTask);
+        // Jetzt wird der Text aus dem Eingabefeld gelöscht
+        inputDOMElement.value = "";
+        /**
+         * Die zentrale Funktion, um die Liste des ToDo-Arrays in den DOM zu rendern
+         * wird wieder getriggert
+         */
+        drawListToDOM();
+    }
+}
+/**
+ * Der check- / unchecked Zustand eines ToDo wird wie folgt gesetzt:
+ */
+function toggleCheckState(index) {
+    /**
+     * Das Array, , das den Checked- / Uncheck-Status der ToDos abbildet,
+     * muss an jener Stelle, an der das entsprechende ToDo steht (nämlich
+     * an der übergebenen Index-Stelle) geändert werden.
+     * Von checked zu unchecked bzw. von unchecked zu checked
+     * Hier wird ein Boolean für den Zustand checked/unchecked genutzt,
+     * der Boolean muss also von true zu false bzw. false zu true gestellt werden.
+     * Ein toggle (also Welchseln zwischen zwei Zuständen) lässt sich folgendermaßen
+     * kurz schreiben: wert = !wert
+     * Bedeutet: der Wert soll das Gegenteil von seinem Wert annehmen.
+     * Alternativ könnte man hier natürlich auch andere Schreibweisen (wie sie im
+     * Kurs behandelt wurden) nutzen.
+     */
+    allTasks[index].todosChecked = !allTasks[index].todosChecked;
+    /**
+     * Die zentrale Funktion, um die Liste des ToDo-Arrays in den DOM zu rendern
+     * wird wieder getriggert
+     */
+    drawListToDOM();
+}
+/**
+ * Diese Funktion löscht ein ToDo
+ */
+function deleteTodo(index) {
+    /**
+     * Durch "index" ist die entsprechende Stelle im Array
+     * bekannt, an der das ToDo steht.
+     * Jetzt muss diese Stelle beider Arrays gelöscht werden,
+     * das ToDo-Text-Array und das Checked/Unchecked-Array
+     */
+    allTasks.splice(index, 1);
+    /**
+     * Die zentrale Funktion, um die Liste des ToDo-Arrays in den DOM zu rendern
+     * wird wieder getriggert
+     */
+    drawListToDOM();
+}
+function addByVoice(input) {
+    if (input != "") {
+        newTask = { todosText: input, todosChecked: false };
+        allTasks.unshift(newTask);
+        inputDOMElement.value = "";
+        drawListToDOM();
+    }
+}
+function checkAllTasks() {
+    for (var v = 0; v < allTasks.length; v++) {
+        allTasks[v].todosChecked = true;
+    }
+    drawListToDOM();
+}
+window.addEventListener("load", function () {
+    var artyom = new Artyom();
+    artyom.addCommands({
+        indexes: ["Erstelle Aufgabe *"],
+        smart: true,
+        action: function (i, wildcard) {
+            addByVoice(wildcard);
+        }
+    });
+    artyom.addCommands({
+        indexes: ["Markiere alle Aufgaben als erledigt"],
+        smart: false,
+        action: function (i) {
+            checkAllTasks();
+        }
+    });
+    function startContinuousArtyom() {
+        artyom.fatality();
+        setTimeout(function () {
+            artyom.initialize({
+                lang: "de-DE",
+                continuous: true,
+                listen: true,
+                interimResults: true,
+                debug: true
+            }).then(function () {
+                console.log("Ready!");
+            });
+        }, 250);
+    }
+    startContinuousArtyom();
+});
+//# sourceMappingURL=script.js.map
--- Aufgabe_9/typescript_9.ts
+++ Aufgabe_9/typescript_9.ts
@@ -1,69 +1,69 @@
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
-    
 
-    
+
+
     var allTasks: tasks[] = [];
     interface tasks {
         taskName: string;
         completed: boolean;
     }
 
-    document.querySelector("#addTask").addEventListener("click", function() {
-        if((<HTMLInputElement> document.getElementById("inputField")).value != " "){
+    document.querySelector("#addTask").addEventListener("click", function () {
+        if ((<HTMLInputElement>document.getElementById("inputField")).value != " ") {
             var newTask = new Object as tasks;
-            newTask.taskName =  (<HTMLInputElement> document.getElementById("inputField")).value;
+            newTask.taskName = (<HTMLInputElement>document.getElementById("inputField")).value;
             allTasks.push(newTask);
             console.log(allTasks.length);
-            (<HTMLInputElement> document.getElementById("inputField")).value = " ";
+            (<HTMLInputElement>document.getElementById("inputField")).value = " ";
             createElementsFromArray();
         }
-    },false);
+    }, false);
 
-    function getTotalNumber(){
+    function getTotalNumber() {
         document.querySelector("#numberOfTasks").innerHTML = allTasks.length + " in total";
     }
 
-    function createElementsFromArray(){
+    function createElementsFromArray() {
         document.getElementById("myTasks").innerHTML = " ";
         var div: any;
-        for(let i = 0; i < allTasks.length; i++){
+        for (let i = 0; i < allTasks.length; i++) {
             console.log(allTasks[i]);
             div = document.createElement("div");
             div.setAttribute("class", "newDiv");
-            div.innerHTML = "<i  class='far fa-circle circle'></i><p class='p'>"+ allTasks[i].taskName+"</p><i class='far fa-trash-alt trash' id='"+i+"'></i>";
+            div.innerHTML = "<i  class='far fa-circle circle'></i><p class='p'>" + allTasks[i].taskName + "</p><i class='far fa-trash-alt trash' id='" + i + "'></i>";
             document.getElementById("myTasks").appendChild(div);
-            document.getElementById(i.toString()).addEventListener("click", function(){
+            document.getElementById(i.toString()).addEventListener("click", function () {
                 deleteObjFromArray(i);
-            },false)
+            }, false)
         }
         let allCircles = document.getElementsByClassName("circle");
-        for(let y = 0; y<allCircles.length; y++){
-            allCircles[y].addEventListener("click", function(){
+        for (let y = 0; y < allCircles.length; y++) {
+            allCircles[y].addEventListener("click", function () {
                 checkItem(this);
             }, false)
         }
         getTotalNumber();
     }
 
-    
-    function deleteObjFromArray(para){
-        allTasks.splice(para, 1);  
-       createElementsFromArray();  
+
+    function deleteObjFromArray(para) {
+        allTasks.splice(para, 1);
+        createElementsFromArray();
     }
 
-    function checkItem(obj){
-        
-        if(obj.classList.contains("fa-circle")){
+    function checkItem(obj) {
+
+        if (obj.classList.contains("fa-circle")) {
             obj.classList.remove("fa-circle");
             obj.classList.add("fa-check-circle");
-        } else{
+        } else {
             obj.classList.add("fa-circle");
             obj.classList.remove("fa-check-circle");
         }
     }
-},false);
+}, false);
 
 
 
